f2182eb62e8db75dd79c9dae8e76a0d726d5ca39
Hajin Kim <enro302@naver.com>
2019-05-07 15:35:12 +0900

    #1 폼 디자이너로 폼 디자인 변경

--- StringIO_TestView/Form1.Designer.cs
+++ StringIO_TestView/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtEdit = new System.Windows.Forms.TextBox();
+            this.입력 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEdit
+            // 
+            this.txtEdit.Location = new System.Drawing.Point(13, 13);
+            this.txtEdit.Name = "txtEdit";
+            this.txtEdit.Size = new System.Drawing.Size(775, 21);
+            this.txtEdit.TabIndex = 0;
+            // 
+            // 입력
+            // 
+            this.입력.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.입력.Location = new System.Drawing.Point(13, 53);
+            this.입력.Name = "입력";
+            this.입력.Size = new System.Drawing.Size(75, 23);
+            this.입력.TabIndex = 1;
+            this.입력.Text = "입력";
+            this.입력.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.입력.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "결과 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(803, 456);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.입력);
+            this.Controls.Add(this.txtEdit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "문자 입출력";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtEdit;
+        private System.Windows.Forms.Button 입력;
+        private System.Windows.Forms.Label label1;
     }
 }
 
